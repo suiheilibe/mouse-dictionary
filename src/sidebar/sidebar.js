942cf263f027aeecb4a9ad85bcb6aa2672868bbb
suiheilibe <ntjapanize@yahoo.co.jp>
2020-02-11 16:01:48 +0900

    sidebar update logic

--- src/sidebar/sidebar.js
+++ src/sidebar/sidebar.js
@@ -3,3 +3,20 @@ let myWindowId = null;
 browser.windows.getCurrent({populate: true}).then((windowInfo) => {
   myWindowId = windowInfo.id;
 });
+
+let container = document.getElementById("container");
+
+let update = (newDom) => {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+  container.appendChild(newDom);
+};
+
+browser.runtime.onMessage.addEventListener(
+  (message) => {
+    if (message.type == "updateSidebar") {
+      update(message.value);
+    }
+  }
+);
